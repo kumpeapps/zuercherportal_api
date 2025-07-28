2036bfebb2850cd70a6fd72eb063b3e05645a883
Justin Kumpe <jakumpe@kumpes.com>
2025-07-28 11:19:09 -0500

    corrected tests

--- tests.py
+++ tests.py
@@ -7,14 +7,14 @@ class LearningCase(unittest.TestCase):
     """Test Cases"""
     def test_benton_county_ar(self):
         """Test Benton County Arkansas"""
-        jail_api = zuercherportal.API(zuercherportal.Jails.AR.BENTON_COUNTY)
+        jail_api = zuercherportal.API(zuercherportal.Jails.AR.BentonCounty)
         inmate_data = jail_api.inmate_search()
         records_count = inmate_data["total_record_count"]
         self.assertGreater(records_count, 0)
 
     def test_pulaski_county_ar(self):
         """Test Pulaski County Arkansas"""
-        jail_api = zuercherportal.API(zuercherportal.Jails.AR.PULASKI_COUNTY)
+        jail_api = zuercherportal.API(zuercherportal.Jails.AR.PulaskiCounty)
         inmate_data = jail_api.inmate_search()
         records_count = inmate_data["total_record_count"]
         self.assertGreater(records_count, 0)
